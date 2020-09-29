157d3e440e12a0d0dddf9e36564347c67856bb30
unknown <oubaniayoub@gmail.com>
2020-09-29 18:44:38 +0100

    completee changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center mt-5 ">
+    <div class="row justify-content-center my-5 ">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
